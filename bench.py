404bd99b5498489b3490f851db8c501d4bced5f7
efsotr <efsotr_l@stu.pku.edu.cn>
2026-06-01 05:41:04 +0000

    Share benchmark result loop

--- bench.py
+++ bench.py
@@ -76,6 +76,52 @@ def benchmark_call(fn, args):
     return ms, fn()
 
 
+def make_results(name, args, sm_count, **metadata):
+    results = {
+        "name": name,
+        "sm_count": sm_count,
+        "dim": args.dim,
+        "results": [],
+    }
+    results.update(metadata)
+    return results
+
+
+def make_base_case(args, bsz, *, fp8_max):
+    weight = make_w(bsz, args.dim)
+    global_scale, global_scale_inv = get_nvfp4_global_scale(weight, FP8_MAX=fp8_max)
+    return {
+        "weight": weight,
+        "global_scale": global_scale,
+        "global_scale_inv": global_scale_inv,
+    }
+
+
+def append_error_result(results, case, ms, scale, code, extra_metrics=None):
+    weight = case["weight"]
+    reconstructed = dequantize("base", code, scale, case["global_scale"])
+    mse, max_abs_error = error_stats(weight, reconstructed)
+
+    row = {
+        "bsz": weight.shape[0],
+        "dim": weight.shape[1],
+        "latency_ms": ms,
+        "mse": mse,
+        "max_abs_error": max_abs_error,
+    }
+    if extra_metrics is not None:
+        row.update(extra_metrics(case, reconstructed))
+    results["results"].append(row)
+
+
+def run_quantize_benchmark(args, results, bsz_list, make_case, quantize, extra_metrics=None):
+    for bsz in bsz_list:
+        case = make_case(bsz)
+        ms, (scale, code) = benchmark_call(lambda: quantize(case), args)
+        append_error_result(results, case, ms, scale, code, extra_metrics)
+    return results
+
+
 def run_mse_benchmark(args, sm_count, *, no_convert):
     if no_convert:
         name = "triton.ScaleSweep_MSE_no_convert"
@@ -89,164 +135,125 @@ def run_mse_benchmark(args, sm_count, *, no_convert):
         lower_bound = SCALESWEEP_MSE_LOWER_BOUND
         upper_bound = SCALESWEEP_MSE_UPPER_BOUND
 
-    results = {
-        "name": name,
-        "lower_bound": lower_bound,
-        "upper_bound": upper_bound,
-        "sm_count": sm_count,
-        "dim": args.dim,
-        "results": [],
-    }
+    results = make_results(
+        name,
+        args,
+        sm_count,
+        lower_bound=lower_bound,
+        upper_bound=upper_bound,
+    )
 
-    for bsz in BSZ_LIST:
-        weight = make_w(bsz, args.dim)
-        global_scale, global_scale_inv = get_nvfp4_global_scale(weight, FP8_MAX=256)
-        fn = lambda: quantize(weight, global_scale_inv, BLOCK_SIZE, lower_bound, upper_bound)
-        ms, (scale, code) = benchmark_call(fn, args)
+    return run_quantize_benchmark(
+        args,
+        results,
+        BSZ_LIST,
+        lambda bsz: make_base_case(args, bsz, fp8_max=256),
+        lambda case: quantize(
+            case["weight"],
+            case["global_scale_inv"],
+            BLOCK_SIZE,
+            lower_bound,
+            upper_bound,
+        ),
+    )
 
-        reconstructed = dequantize("base", code, scale, global_scale)
-        mse, max_abs_error = error_stats(weight, reconstructed)
-        results["results"].append(
-            {
-                "bsz": bsz,
-                "dim": weight.shape[1],
-                "latency_ms": ms,
-                "mse": mse,
-                "max_abs_error": max_abs_error,
-            }
-        )
 
-    return results
+def make_weighted_case(args, bsz):
+    case = make_base_case(args, bsz, fp8_max=256)
+    weight = case["weight"]
+    case["imp"] = make_imp(args.imp, weight.shape[1], weight.device)
+    return case
+
+
+def weighted_metrics(case, reconstructed):
+    weighted_mse, _ = weighted_error_stats(case["weight"], reconstructed, case["imp"])
+    return {"weighted_mse": weighted_mse}
 
 
 def run_weighted_benchmark(args, sm_count, *, no_convert):
     if no_convert:
         name = "triton.ScaleSweep_no_convert"
         quantize = scalesweep_no_convert_quantize
-        fallback_quantize = mse_scalesweep_no_convert_quantize
         lower_bound = SCALESWEEP_NO_CONVERT_LOWER_BOUND
         upper_bound = SCALESWEEP_NO_CONVERT_UPPER_BOUND
     else:
         check_sm100()
         name = "triton.ScaleSweep"
         quantize = scalesweep_quantize
-        fallback_quantize = None
         lower_bound = SCALESWEEP_LOWER_BOUND
         upper_bound = SCALESWEEP_UPPER_BOUND
 
-    results = {
-        "name": name,
-        "lower_bound": lower_bound,
-        "upper_bound": upper_bound,
-        "sm_count": sm_count,
-        "dim": args.dim,
-        "imp": args.imp,
-        "results": [],
-    }
+    results = make_results(
+        name,
+        args,
+        sm_count,
+        lower_bound=lower_bound,
+        upper_bound=upper_bound,
+        imp=args.imp,
+    )
 
-    for bsz in WEIGHTED_BSZ_LIST:
-        weight = make_w(bsz, args.dim)
-        imp = make_imp(args.imp, weight.shape[1], weight.device)
-        global_scale, global_scale_inv = get_nvfp4_global_scale(weight, FP8_MAX=256)
-
-        if no_convert and args.imp == "ones":
-            fn = lambda: fallback_quantize(
-                weight,
-                global_scale_inv,
-                BLOCK_SIZE,
-                lower_bound,
-                upper_bound,
-            )
-        else:
-            fn = lambda: quantize(
-                weight,
-                imp,
-                global_scale_inv,
-                BLOCK_SIZE,
-                lower_bound,
-                upper_bound,
-            )
-        ms, (scale, code) = benchmark_call(fn, args)
-
-        reconstructed = dequantize("base", code, scale, global_scale)
-        mse, max_abs_error = error_stats(weight, reconstructed)
-        weighted_mse, _ = weighted_error_stats(weight, reconstructed, imp)
-        results["results"].append(
-            {
-                "bsz": bsz,
-                "dim": weight.shape[1],
-                "latency_ms": ms,
-                "mse": mse,
-                "weighted_mse": weighted_mse,
-                "max_abs_error": max_abs_error,
-            }
-        )
-
-    return results
+    return run_quantize_benchmark(
+        args,
+        results,
+        WEIGHTED_BSZ_LIST,
+        lambda bsz: make_weighted_case(args, bsz),
+        lambda case: quantize(
+            case["weight"],
+            case["imp"],
+            case["global_scale_inv"],
+            BLOCK_SIZE,
+            lower_bound,
+            upper_bound,
+        ),
+        weighted_metrics,
+    )
 
 
 def run_absmax_benchmark(args, sm_count):
-    results = {
-        "name": "triton.AbsMax_no_convert",
-        "block_size": BLOCK_SIZE,
-        "fp8_max": args.fp8_max,
-        "sm_count": sm_count,
-        "dim": args.dim,
-        "results": [],
-    }
+    results = make_results(
+        "triton.AbsMax_no_convert",
+        args,
+        sm_count,
+        block_size=BLOCK_SIZE,
+        fp8_max=args.fp8_max,
+    )
 
-    for bsz in BSZ_LIST:
-        weight = make_w(bsz, args.dim)
-        global_scale, global_scale_inv = get_nvfp4_global_scale(weight, FP8_MAX=args.fp8_max)
-        fn = lambda: absmax_quantize_no_convert(weight, global_scale_inv, BLOCK_SIZE)
-        ms, (scale, code) = benchmark_call(fn, args)
-
-        reconstructed = dequantize("base", code, scale, global_scale)
-        mse, max_abs_error = error_stats(weight, reconstructed)
-        results["results"].append(
-            {
-                "bsz": bsz,
-                "dim": weight.shape[1],
-                "latency_ms": ms,
-                "mse": mse,
-                "max_abs_error": max_abs_error,
-            }
-        )
-
-    return results
+    return run_quantize_benchmark(
+        args,
+        results,
+        BSZ_LIST,
+        lambda bsz: make_base_case(args, bsz, fp8_max=args.fp8_max),
+        lambda case: absmax_quantize_no_convert(
+            case["weight"],
+            case["global_scale_inv"],
+            BLOCK_SIZE,
+        ),
+    )
 
 
 def run_vllm_benchmark(args, sm_count):
     from vllm._custom_ops import scaled_fp4_quant
 
     check_sm100()
-    results = {
-        "name": "triton.vllm",
-        "sm_count": sm_count,
-        "dim": args.dim,
-        "results": [],
-    }
+    results = make_results("triton.vllm", args, sm_count)
 
-    for bsz in BSZ_LIST:
-        weight = make_w(bsz, args.dim)
-        global_scale, global_scale_inv = get_nvfp4_global_scale(weight)
-        fn = lambda: scaled_fp4_quant(weight, global_scale_inv)
-        ms, (code, scale) = benchmark_call(fn, args)
-        scale = unswizzle_vllm_fp4_scale(scale, m=weight.shape[0], n=weight.shape[1], block_size=BLOCK_SIZE)
-
-        reconstructed = dequantize("base", code, scale, global_scale)
-        mse, max_abs_error = error_stats(weight, reconstructed)
-        results["results"].append(
-            {
-                "bsz": bsz,
-                "dim": weight.shape[1],
-                "latency_ms": ms,
-                "mse": mse,
-                "max_abs_error": max_abs_error,
-            }
+    def quantize(case):
+        code, scale = scaled_fp4_quant(case["weight"], case["global_scale_inv"])
+        scale = unswizzle_vllm_fp4_scale(
+            scale,
+            m=case["weight"].shape[0],
+            n=case["weight"].shape[1],
+            block_size=BLOCK_SIZE,
         )
+        return scale, code
 
-    return results
+    return run_quantize_benchmark(
+        args,
+        results,
+        BSZ_LIST,
+        lambda bsz: make_base_case(args, bsz, fp8_max=448.0),
+        quantize,
+    )
 
 
 def run_benchmark(name, args, sm_count):
